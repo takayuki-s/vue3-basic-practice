a3be30bc43942342f8bd97c1ea3ad28fc1c82aab
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-17 06:48:12 +0900

    タスク追加

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
   data: () => ({
     newItem: "",
+    todos: [],
   }),
   methods: {
     addItem(event) {
       alert("Clicked!");
+      let todo = {
+        item: this.newItem,
+      };
+      this.todos.push(todo);
     },
   },
 });
