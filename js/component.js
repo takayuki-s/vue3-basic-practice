90cb8fc95d9ee02294354ff41f8fc2f6ecf1398e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-28 06:15:32 +0900

    コンポーネントで動的処理
    コンポーネントを作成する度に、新しいインスタンスが作成されるため、それぞれが独自にカウントアップすることができる

--- js/component.js
+++ js/component.js
@@ -2,10 +2,24 @@ const helloComponent = {
   template: "<p>Hello!</p>",
 };
 
+const buttonCounter = {
+  template:
+    "<div><span>count: </span><button v-on:click='countUp'>{{ count }}</button></div>",
+  data: () => ({
+    count: 0,
+  }),
+  methods: {
+    countUp(event) {
+      this.count++;
+    },
+  },
+};
+
 const app = Vue.createApp({
   data: () => ({}),
   components: {
     "hello-component": helloComponent,
+    "button-counter": buttonCounter,
   },
 });
 
